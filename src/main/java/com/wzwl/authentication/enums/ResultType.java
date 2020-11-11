6a861a0c5837362a61236c217e2e4d004b0590bf
hufei <1551538376@qq.com>
2020-11-11 15:55:10 +0800

    部分完善

--- src/main/java/com/wzwl/authentication/enums/ResultType.java
+++ src/main/java/com/wzwl/authentication/enums/ResultType.java
@@ -1,24 +1,30 @@
 package com.wzwl.authentication.enums;
 
+/**
+ * @author huff
+ * @date 2020/11/11 15:53
+ */
+
 public enum ResultType {
-	SUCCESS("200","请求成功"),FAIL("500","处理失败");
-	
-	private ResultType(String code, String codeDesc) {
-		// TODO Auto-generated constructor stub
-		this.code = code;
-		this.codeDesc = codeDesc;
-	}
-	protected String code;
-	
-	protected String codeDesc;
+    SUCCESS("200","请求成功"),FAIL("500","处理失败");
 
-	public String getCode() {
-		return code;
-	}
+    private ResultType(String code, String codeDesc) {
+        // TODO Auto-generated constructor stub
+        this.code = code;
+        this.codeDesc = codeDesc;
+    }
+    protected String code;
 
-	public String getCodeDesc() {
-		return codeDesc;
-	}
-	
-	
+    protected String codeDesc;
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getCodeDesc() {
+        return codeDesc;
+    }
+
+
 }
+
